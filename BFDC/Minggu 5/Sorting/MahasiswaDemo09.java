621ffb11b4d297d773dc06e4cff70d0578dff146
iyyann14 <iniiyyann@gmail.com>
2026-04-07 12:26:25 +0700

    Jobsheet 6

--- BFDC/Minggu 5/Sorting/MahasiswaDemo09.java
+++ BFDC/Minggu 5/Sorting/MahasiswaDemo09.java
@@ -1,33 +1,23 @@
-import java.util.Scanner;
-
 public class MahasiswaDemo09 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan Jumlah Mahasiswa : ");
-        int jumlah = Integer.parseInt(sc.nextLine());
-        Mahasiswa09[] arrayOfMahasiswa = new Mahasiswa09[jumlah];
-        String dummy;
+        MahasiswaBerprestasi09 list = new MahasiswaBerprestasi09();
+        Mahasiswa09 m1 = new Mahasiswa09("123", "Zidan", "2A", 3.2);
+        Mahasiswa09 m2 = new Mahasiswa09("124", "Ayu", "2A", 3.5);
+        Mahasiswa09 m3 = new Mahasiswa09("125", "Sofi", "2A", 3.1);
+        Mahasiswa09 m4 = new Mahasiswa09("126", "Sita", "2A", 3.9);
+        Mahasiswa09 m5 = new Mahasiswa09("127", "Miki", "2A", 3.7);
 
-        for(int i = 0; i < jumlah; i++) {
-            arrayOfMahasiswa[i] = new Mahasiswa09();
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
 
-            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM : ");
-            arrayOfMahasiswa[i].nim = sc.nextLine();
-            System.out.print("Nama : ");
-            arrayOfMahasiswa[i].nama = sc.nextLine();
-            System.out.print("Kelas : ");
-            arrayOfMahasiswa[i].kelas = sc.nextLine();
-            System.out.print("IPK : ");
-            dummy = sc.nextLine();
-            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
-            System.out.println("----------------------------------");
-        }
+        System.out.println("Data Mahasiswa Sebelum Sorting: ");
+        list.tampil();
 
-        
-        for(int i = 0; i < jumlah; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i + 1));
-            arrayOfMahasiswa[i].tampilkanInformasi();
-        }
+        System.out.println("Data Mahasiswa Setelah Sorting Berdasarkan IPK (DESC): ");
+        list.bubbleSort();
+        list.tampil();
     }
 }
