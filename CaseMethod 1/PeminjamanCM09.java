c5831a0b357a61c834d06f7f5cee91b1aa357b0f
iyyann14 <iniiyyann@gmail.com>
2026-04-14 21:23:27 +0700

    Perubahan

--- CaseMethod 1/PeminjamanCM09.java
+++ CaseMethod 1/PeminjamanCM09.java
@@ -1,33 +1,28 @@
-class PeminjamanCM09 {
-    MahasiswaCM09 mhs;
-    BukuCM09 buku;
-    int lamaPinjam;
-    int denda;
+public class PeminjamanCM09 {
+    public MahasiswaCM09 mhs;
+    public BukuCM09 buku;
+    public int lamaPinjam, terlambat, denda;
+    public int batasPinjam = 5;
 
-    // CONSTRUCTOR PEMINJAMAN
-    PeminjamanCM09(MahasiswaCM09 mhs, BukuCM09 buku, int lamaPinjam) {
+    public PeminjamanCM09(MahasiswaCM09 mhs, BukuCM09 buku, int lamaPinjam) {
         this.mhs = mhs;
         this.buku = buku;
         this.lamaPinjam = lamaPinjam;
         hitungDenda();
     }
 
-    //METHOD MENGHITUNG DENDA
-    void hitungDenda() {
-        int batas = 5;
-        int dendaPerHari = 2000;
-
-        if (lamaPinjam > batas) {
-            int telat = lamaPinjam - batas;
-            denda = telat * dendaPerHari;
+    public void hitungDenda() {
+        terlambat = lamaPinjam - batasPinjam;
+        if (terlambat > 0) {
+            denda = terlambat * 2000;
         } else {
+            terlambat = 0;
             denda = 0;
         }
     }
 
-    //METHOD MENAMPILKAN DATA PEMINJAMAN 
-    void tampil() {
-        System.out.println(mhs.nim + " | " + mhs.nama + " | " + buku.judul +
-                " | " + lamaPinjam + " hari | Denda: " + denda);
+    public void tampil() {
+        System.out.printf("%-10s | %-12s | Lama: %-2d | Terlambat: %-2d | Denda: %-5d\n", 
+            mhs.nama, buku.judul, lamaPinjam, terlambat, denda);
     }
 }
---
+++ CaseMethod 1/ProsesCM09.java
@@ -0,0 +1,96 @@
+public class ProsesCM09 {
+    public MahasiswaCM09[] mhs;
+    public BukuCM09[] buku;
+    public PeminjamanCM09[] pinjam;
+
+    public ProsesCM09() {
+        mhs = new MahasiswaCM09[] {
+                new MahasiswaCM09("22001", "Andi", "Teknik Informatika"),
+                new MahasiswaCM09("22002", "Budi", "Teknik Informatika"),
+                new MahasiswaCM09("22003", "Citra", "Sistem Informasi Bisnis")
+        };
+
+        // DATA BUKU
+        buku = new BukuCM09[] {
+                new BukuCM09("B001", "Algoritma", 2020),
+                new BukuCM09("B002", "Basis Data", 2019),
+                new BukuCM09("B003", "Pemrograman", 2021),
+                new BukuCM09("B004", "Fisika", 2024)
+        };
+
+        // DATA PEMINJAMAN
+        pinjam = new PeminjamanCM09[] {
+                new PeminjamanCM09(mhs[0], buku[0], 7),
+                new PeminjamanCM09(mhs[1], buku[1], 3),
+                new PeminjamanCM09(mhs[2], buku[2], 10),
+                new PeminjamanCM09(mhs[2], buku[3], 6),
+                new PeminjamanCM09(mhs[0], buku[1], 4)
+        };
+
+    }
+
+    static void insertionSortByDenda(PeminjamanCM09[] pinjam) {
+        // INSERTION SORT
+        for (int i = 1; i < pinjam.length; i++) {
+            PeminjamanCM09 temp = pinjam[i];
+            int j = i - 1;
+
+            while (j >= 0 && pinjam[j].denda < temp.denda) {
+                pinjam[j + 1] = pinjam[j];
+                j--;
+            }
+            pinjam[j + 1] = temp;
+        }
+    }
+
+    static void insertionSortByNIM(PeminjamanCM09[] pinjam) {
+        for (int i = 1; i < pinjam.length; i++) {
+            PeminjamanCM09 temp = pinjam[i];
+            int j = i - 1;
+
+            while (j >= 0 &&
+                    Integer.parseInt(pinjam[j].mhs.nim) > Integer.parseInt(temp.mhs.nim)) {
+                pinjam[j + 1] = pinjam[j];
+                j--;
+            }
+            pinjam[j + 1] = temp;
+        }
+    }
+    // DATA MAHASISWA
+
+    static void cariNIM(PeminjamanCM09[] pinjam, String cari) {
+        int left = 0;
+        int right = pinjam.length - 1;
+        boolean ditemukan = false;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int nimMid = Integer.parseInt(pinjam[mid].mhs.nim);
+            int cariI = Integer.parseInt(cari);
+
+            if (nimMid == cariI) {
+                System.out.println("\n=== DATA DITEMUKAN ===");
+                System.out.println("NIM   : " + pinjam[mid].mhs.nim);
+                System.out.println("Nama  : " + pinjam[mid].mhs.nama);
+                System.out.println("Buku  : " + pinjam[mid].buku.judul);
+                System.out.println("Hari  : " + pinjam[mid].lamaPinjam);
+                System.out.println("Denda : " + pinjam[mid].denda);
+                ditemukan = true;
+                break;
+            } else if (nimMid < cariI) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Data tidak ditemukan!");
+        }
+    }
+
+    public static void main(String[] args) {
+        ProsesCM09 proses = new ProsesCM09();
+    }
+}
